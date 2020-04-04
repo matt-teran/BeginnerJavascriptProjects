2cfb399ce5e9b268fdea6b13b851396b7842bc2c
Matthew Teran <matt@Matthews-Air.lan1>
2020-04-04 17:36:47 -0500

    Added Counter project

---
+++ Counter/script.js
@@ -0,0 +1,7 @@
+document.querySelector('#lower').onclick = () => {
+    document.querySelector('#counter').innerHTML--;
+}
+document.querySelector('#add').onclick = () => {
+    document.querySelector('#counter').innerHTML++;
+};
+
